1abd6be41f83299c05cd2a056e98066549c623cf
rico <gugomkh@mail.ru>
2022-08-06 13:14:40 +0300

    тест 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу,
+// которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+
+int [] array = {345, 587, 988, 456, 464, 267, 248, 862, 159, 148}; 
+
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] % 2 == 0) count++;
+}
+Console.WriteLine($"Количество четных чисел {count}");
